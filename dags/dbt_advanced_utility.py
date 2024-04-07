dbfdf78fa45fb297f06ae6c33d034767e42c5a60
Ed Posinitskii <edposinitskii@double.cloud>
2024-04-07 12:46:44 +0200

    * use relative to dags path for includes

--- dags/dbt_advanced_utility.py
+++ dags/dbt_advanced_utility.py
@@ -9,7 +9,8 @@ from pendulum import datetime
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
-from .include.dbt_dag_parser import DbtDagParser
+# $AIRFLOW_HOME/dags is added to sys.path so we can use modules relative to `dags` root
+from include.dbt_dag_parser import DbtDagParser
 
 # We're hardcoding these values here for the purpose of the demo, but in a production environment these
 # would probably come from a config file and/or environment variables!
