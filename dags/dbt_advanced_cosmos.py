047497f87a9186241d8b09e8a1d66010c2d5813b
Ed Posinitskii <edposinitskii@double.cloud>
2024-04-07 15:51:42 +0200

    * cosmos using custom clickhouse profile

--- dags/dbt_advanced_cosmos.py
+++ dags/dbt_advanced_cosmos.py
@@ -9,16 +9,11 @@ from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
 
+from include.clickhouse_profile import ClickhouseUserPasswordProfileMapping
+
 # We're hardcoding this value here for the purpose of the demo, but in a production environment this
 # would probably come from a config file and/or environment variables!
 DBT_PROJECT_DIR = Path("/usr/app/dbt")
-DBT_CONNECTION_ENV = {
-    "DBT_USER": "{{ conn.clickhouse_dwh.login }}",
-    "DBT_ENV_SECRET_PASSWORD": "{{ conn.clickhouse_dwh.password }}",
-    "DBT_HOST": "{{ conn.clickhouse_dwh.host }}",
-    "DBT_SCHEMA": "{{ conn.clickhouse_dwh.schema }}",
-    "DBT_PORT": "{{ conn.clickhouse_dwh.port }}",
-}
 
 with DAG(
         "dbt_advanced_cosmos",
@@ -37,13 +32,14 @@ with DAG(
         group_id="my_jaffle_shop_project",
         project_config=ProjectConfig(
             dbt_project_path=DBT_PROJECT_DIR,
-            env_vars=DBT_CONNECTION_ENV,
         ),
         profile_config=ProfileConfig(
-            # these map to dbt/profiles.yml
             profile_name="jaffle_shop",
             target_name="dev",
-            profiles_yml_filepath=DBT_PROJECT_DIR / "profiles.yml",
+            profile_mapping=ClickhouseUserPasswordProfileMapping(
+                conn_id="clickhouse_dwh",
+                profile_args={"dbname": "jaffle_shop"},
+            ),
         ),
         execution_config=ExecutionConfig(),
     )
---
+++ dags/include/clickhouse_profile.py
@@ -0,0 +1,56 @@
+"""
+Maps Airflow SQLite connections using user + password authentication to dbt clickhouse profile.
+"""
+
+from __future__ import annotations
+
+from typing import Any
+
+from cosmos.profiles.base import BaseProfileMapping
+
+
+class ClickhouseUserPasswordProfileMapping(BaseProfileMapping):
+    """
+    Maps Airflow SQLite connections using user + password authentication to dbt Clickhouse profiles.
+    https://docs.getdbt.com/docs/core/connect-data-platform/clickhouse-setup
+    """
+
+    airflow_connection_type: str = "sqlite"
+    default_port = 9443
+
+    required_fields = [
+        "host",
+        "login",
+        "schema",
+        "clickhouse",
+    ]
+    secret_fields = [
+        "password",
+    ]
+    airflow_param_mapping = {
+        "host": "host",
+        "login": "login",
+        "password": "password",
+        "port": "port",
+        "schema": "schema",
+        "clickhouse": "extra.clickhouse",
+    }
+
+    @property
+    def profile(self) -> dict[str, Any | None]:
+        "Gets profile. The password is stored in an environment variable."
+        profile = {
+            "type": "clickhouse",
+            "schema": self.conn.schema,
+            "user": self.conn.login,
+            # password should always get set as env var
+            "password": self.get_env_var_format("password"),
+            "driver": self.conn.extra_dejson.get("driver") or "http",
+            "port": self.conn.port or self.default_port,
+            "host": self.conn.host,
+            "secure": self.conn.extra_dejson.get("secure") or True,
+            "verify": self.conn.extra_dejson.get("verify") or False,
+            **self.profile_args,
+        }
+
+        return self.filter_null(profile)
